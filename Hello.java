5f3cb386887870b035363205f388196461addf51
Mikhail <baryshev-03@mail.ru>
2023-10-10 17:32:34 +0300

    gh

--- Hello.java
+++ Hello.java
@@ -1,9 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
-
-	System.out.println("1\n");
+	System.out.println("A");
         	System.out.println("Hello world!");
-	System.out.println("2\n");
+	
 
     }
 }
